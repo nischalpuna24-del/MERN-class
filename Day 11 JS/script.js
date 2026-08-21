01689269d96f1d8f2063089dc4a7a3b4b9db8651
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-08-21 22:01:28 +0545

    Js

---
+++ Day 11 JS/script.js
@@ -0,0 +1,31 @@
+// alert('Hell0')
+// document.write('Hello')
+console.log('Startin Js.... here is day 11')
+
+// var  =  functional scope
+// let = bloack scope
+// const = block scope
+
+// variable --> container
+
+var a = 30
+console.log(a)
+
+
+// data types: number, string, boolean, boject, null
+
+console.log(20)
+console.log("Hello there")
+console.log(2==3)
+
+// types of comments:
+/* Hello how are
+you ? */
+// typeof
+
+
+// var a = {
+//     "name":"ram",
+//     "age":40
+// }
+// console.log(a)
