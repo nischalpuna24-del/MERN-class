ccc54c14078417a6195c42ec4029ee95856a864c
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-08-26 11:28:27 +0545

    Assignment completed

--- Day 14/assignment.js
+++ Day 14/assignment.js
@@ -40,7 +40,7 @@
 // let items2 = [1, 23, 4, 55, 7, 8];
 // items2.sort((a, b) => {
 //     return a - b
-// }) 
+// })
 // items2.reverse();
 // console.log(items2);
 
@@ -62,23 +62,48 @@
 //     console.log(arr2)
 
 // 11. use find() to get the first number less than 10:
-	let num = [12,34,56,7,88,9]
+// let num = [12,34,56,7,88,9]
+// let find = num.find((ele)=> {
+// 	return ele<10
+// })
+// console.log(find)
 
-// 12. use some() to check if any student has scored below 40
+// // 12. use some() to check if any student has scored below 40
 // 	let marks = [45,67,89,30]
+// 	let check = marks.some((ele)=> {
+// 		return ele<40
+// 	})
+// 	console.log(check)
 
 // 13. use every() to check if all numbers are even:
-// 	let numbers = [2,4,6,8,12]
+// let numbers = [2,4,6,8,12]
+// let check = numbers.every((ele)=> {
+// 	return ele%2===0
+// })
+// console.log(check)
 
-// 14. Destructure this array to get firstName and lastName
+// // 14. Destructure this array to get firstName and lastName
 // 	let name = ["Ram","Prasad","Sharma"]
+// 	let [firstname,,lastname] = name
+// 	console.log(firstname,lastname)
 
 // 15. merge two array using spread operator:
-// 	let a = [1,2,4]
-// 	let b = [3,5,6]
+// let a = [1,2,4]
+// let b = [3,5,6]
+// let arr = [...a,...b]
+// console.log(arr)
 
 // 16. Add "Football" to the start of this array using spread:
-// 	let sports = ["cricket","basketball"]
+// let sports = ["cricket", "basketball"];
+// let arr = ["Football", ...sports];
+// // console.log(arr.unshift("Football"))
+// console.log(arr);
 
 // 17. clone this array not by reference.
-// 	// let arr3 = [1,2,3]
+let arr3 = [1, 2, 3];
+let arr2 = [...arr3];
+
+arr2.push(5);
+
+console.log(arr3);
+console.log(arr2);
