37dac6e5ae1c41351f2dfd1c800830823eb8e4bf
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-08-26 20:51:37 +0545

    Oject Completed

--- Day 15/object.js
+++ Day 15/object.js
@@ -1,27 +1,71 @@
-console.log("object in day 15")
+console.log("object in day 15");
 
-const obj = {
-    name: "ram",
-    age: 12,
-    isAdmin:false
-}
+// const obj = {
+//     name: "ram",
+//     age: 12,
+//     isAdmin:false
+// }
 
-obj.name = "shyam"
-console.log(obj)
-// console.log(obj.age)
-console.log(obj['age'])
+// accessing the obejct keys and value
+// obj.name = "shyam"
+// console.log(obj)
+// // console.log(obj.age)
+// console.log(obj['age'])
+
+// // NESTED OBJECT
+// const user = {
+//     name: "ram",
+//     age: 30,
+//     location: {
+//         city: "kathmandu",
+//         postalcode: 466000,
+//     }
+// }
+
+// // OPTIONAL CHAINING ?
+// // console.log(user?.location?.city)
+
+// // DESTRUCTURING
+// const { city, postalcode} = user.location
+
+// console.log(city,postalcode)
+
+// const obj2 = {
+//     name: 'ram',
+//     age: 20,
+//     email: 'ram@gmail.com'
+// }
+
+// for in loop in object
+// for (const key in obj2) {
+//     const element = obj2[key];   
+//     console.log(key,element)
+// }
 
 
-// NESTED OBJECT
-const user = {
-    name: "ram",
-    age: 30,
-    location: {
-        city: "kathmandu",
-        postalcode: 466000,
-    }
-}
+// object.entries
+// const obj2 = {
+//     name: 'ram',
+//     age: 20,
+//     email: 'ram@gmail.com'
+// }
+// console.log(Object.entries(obj2))
 
-console.log(user?.location.city)
 
-// OPTIONAL CHAINING ?
+// spread operator
+
+// const obj2 = {
+//     name: 'ram',
+//     age: 20,
+//     email: 'ram@gmail.com', 
+//     location: {
+//         city: "kathmandu",
+//         postalcode:46000
+//     }
+// }
+
+// let obj3 = {...obj2}
+// // obj3.name = 'ramuuuu'
+// console.log(obj3, obj2)
+
+ 
