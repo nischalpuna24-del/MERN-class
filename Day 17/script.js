7682b94ff940cce19141ca1f52db541fab5e3337
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-08-30 20:45:28 +0545

    download file created

---
+++ Day 17/script.js
@@ -0,0 +1,62 @@
+// let container = document.querySelector(".container")
+// let boxcontainer = document.querySelector(".boxcontainer")
+// let box = document.querySelector(".box")
+
+// container.addEventListener("click",()=>{
+//     alert("I am a cotianer")
+// })
+
+// boxcontainer.addEventListener("click",()=>{
+//     alert("I am a box-cotianer")
+// })
+// box.addEventListenercontainer.addEventListener("click",(e)=>{
+//     e.stopPropagation()
+//     alert("I am a cotianer")
+// })
+
+// let uksername = document.querySelector("input")
+// let form = document.querySelector("form")
+// let span = document.querySelector("span")
+
+// const usernameRegex =/^[A-Za-z]{3,}$/;
+
+// form.addEventListener("submit", (e)=>{
+//     e.preventDefault()
+//     console.dir(username.value)
+
+//     if(!usernameRegex,test(username.value))
+//         return span.innerText = "Username is invlaid"
+
+//     span.innerText = ""
+//     username.value = ""
+// })
+
+// timers and intervals
+// setTimeout and setInterval
+
+// setTimeout(()=>{
+//     alert("Hello from set timeout ")
+// }, 2000)
+
+// let interval = setInterval(() => {
+//     console.log("From the interval")
+// }, 2000);
+
+// clearInterval(interval)
+
+// let count = 10;
+// let interval = setInterval(() => {
+//   if (count >= 1) {
+//     count--;
+//     console.log(count);
+//   } else {
+//     clearInterval(interval);
+//   }
+// }, 1000);
+
+
+// downloading file progress bar
+
+let interval = setInertval(()=> {
+
+}, 3000)
