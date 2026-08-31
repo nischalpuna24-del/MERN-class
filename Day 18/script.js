c32b0c01e1ac898b841321a1beeddaca0162416f
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-08-31 21:45:51 +0545

    API files created

--- Day 18/script.js
+++ Day 18/script.js
@@ -1,4 +1,4 @@
-console.log("Day 18 OOP")
+console.log("Day 18 OOP");
 
 // let bricks1 = {
 //     name: "abc",
@@ -30,4 +30,76 @@ console.log("Day 18 OOP")
 //     qty: 10
 // }
 
+// class and object
+// class --> blueprint
+// // object --> instance - created from class
 
+// class BricksFacotry {
+//     constructor(name, qty, color){
+//         this.name = name
+//         this.qyt = qty
+//         this.color = color
+
+//         console.log({name:this.name,qty:this.qty,color:this.color})
+//     }
+
+//     // methods
+//     getQuantity() {
+//         return this.qty
+//     }
+
+//     setQuantity(value) {
+//         return this.qty = value
+//     }
+// }
+
+// let b1 = new BricksFacotry("abc",30,"red")
+// let b2 = new BricksFacotry("abc",30,"green")
+// let b3 = new BricksFacotry("abc",30,"blue")
+
+// // Inhertitance
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   eat() {
+//     console.log("eating...");
+//   }
+// }
+
+// // let a1 = new Animal()
+
+// class Dog extends Animal {
+//   constructor(name, age) {
+//     super(name);
+//     this.age = age;
+//   }
+//   eat() {
+//     console.log("dog is eating");
+//   }
+// }
+
+// let d1 = new Dog("tommy", 13);
+
+// async / sync js
+
+
+console.log(1)
+console.log(2)
+
+// setTimeout(() => {
+//   console.log(3)
+// }, 2000)
+
+console.log(4)
+
+// callbacks
+
+function greet(value,cb) {
+    console.log(value)
+    cb()
+}
+
+greet("hello", function good(){
+    console.log("I am god")
+})
