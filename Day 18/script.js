49714e0cf390463a13934a0d3482a405e4e24abc
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-08-31 20:26:16 +0545

    Day 18 OPP files added

---
+++ Day 18/script.js
@@ -0,0 +1,33 @@
+console.log("Day 18 OOP")
+
+// let bricks1 = {
+//     name: "abc",
+//     color: 'red',
+//     qty: 10
+// }
+
+// let bricks2 = {
+//     name: "abc",
+//     color: 'orange',
+//     qty: 30
+// }
+
+// let bricks3 = {
+//     name: "abc",
+//     color: 'green',
+//     qty: 50
+// }
+
+// let bricks4 = {
+//     name: "abc",
+//     color: 'red',
+//     qty: 10
+// }
+
+// let bricks5 = {
+//     name: "abc",
+//     color: 'red',
+//     qty: 10
+// }
+
+
