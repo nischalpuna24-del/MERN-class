5d9e5b2020ef8149db7ba7f206b53a9576de41cd
Nischal Pun <nischal.pun.a24@icp.edu.np>
2026-09-07 20:14:26 +0545

    Day 20-REACT

--- Day 20/import.js
+++ Day 20/import.js
@@ -1,1 +1,12 @@
 const b = 6
+const c = 7
+const d = 8
+
+const obj = {
+    name: "John",
+    age: 30
+}
+
+export default obj 
+
+export {b}
--- Day 20/script.js
+++ Day 20/script.js
@@ -1,5 +1,11 @@
 console.log("import and export")
 
+import obj from './import.js'
+import {b,c,d} from './import.js'
+
 const a = 5
 console.log(a)
+console.log(obj)
 console.log(b)
+console.log(c)
+console.log(d)
